3f6a29ee21a3f43a33435ab32a422cb0c3165db7
shudiptod <shudiptod@gmail.com>
2026-01-08 02:06:19 +0600

    updated

--- src/controllers/admin.auth.controller.ts
+++ src/controllers/admin.auth.controller.ts
@@ -44,14 +44,14 @@ export const loginAdmin = async (req: AuthRequest, res: Response) => {
     );
 
     // send token in http only cookie 
-    res.cookie("token", token, {
+    res.cookie("authToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
-    // res.json({ success: true, message: "Logged in successfully", token, id: admin.id, email: admin.email, role: admin.role });
+    res.json({ success: true, message: "Logged in successfully", token, id: admin.id, email: admin.email, role: admin.role });
   } catch (err: any) {
     console.error(err);
     res.status(500).json({ success: false, message: err.message });
