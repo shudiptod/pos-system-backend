884b35fe44491ef605ad380e2e2dd0dbe98bb97f
shudiptod <shudiptod@gmail.com>
2026-01-21 21:00:38 +0600

    customer image

--- src/routes/upload.router.ts
+++ src/routes/upload.router.ts
@@ -1,6 +1,7 @@
 
 
 
+import { authenticateCustomer } from "../middleware/customerAuth";
 import { uploadFile } from "../controllers/upload.controller";
 import { authenticateJWT, authorize } from "../middleware/auth";
 import { upload } from "../middleware/upload";
@@ -13,6 +14,13 @@ router.post(
     "/",
     authenticateJWT as any,
     authorize(["ADMIN", "SUPER_ADMIN", "MANAGER"]) as any,
+    upload.single("file"), // Expect field name 'file'
+    uploadFile
+);
+
+router.post(
+    "/customer",
+    authenticateCustomer as any,
     upload.single("file"), // Expect field name 'file'
     uploadFile
 );
